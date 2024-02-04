06412278a96b1776b0a288eabc5d4f929138ee7a
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-02-04 20:38:55 +0000

    adding links, fixup nav bar

---
+++ next-portfolio-website/src/app/contact/page.tsx
@@ -0,0 +1,68 @@
+const Contact = () => {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-5xl font-bold text-center">Lorem Ipsum</h1>
+          <p className="text-center">
+            "On the other hand, we denounce with righteous indignation and dislike
+            men who are so beguiled and demoralized by the charms of pleasure of
+            the moment, so blinded by desire, that they cannot foresee the pain
+            and trouble that are bound to ensue; and equal blame belongs to those
+            who fail in their duty through weakness of will, which is the same as
+            saying through shrinking from toil and pain. These cases are perfectly
+            simple and easy to distinguish. In a free hour, when our power of
+            choice is untrammelled and when nothing prevents our being able to do
+            what we like best, every pleasure is to be welcomed and every pain
+            avoided. But in certain circumstances and owing to the claims of duty
+            or the obligations of business it will frequently occur that pleasures
+            have to be repudiated and annoyances accepted. The wise man therefore
+            always holds in these matters to this principle of selection: he
+            rejects pleasures to secure other greater pleasures, or else he
+            endures pains to avoid worse pains."
+          </p>
+        </div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-5xl font-bold text-center">Lorem Ipsum</h1>
+          <p className="text-center">
+            "On the other hand, we denounce with righteous indignation and dislike
+            men who are so beguiled and demoralized by the charms of pleasure of
+            the moment, so blinded by desire, that they cannot foresee the pain
+            and trouble that are bound to ensue; and equal blame belongs to those
+            who fail in their duty through weakness of will, which is the same as
+            saying through shrinking from toil and pain. These cases are perfectly
+            simple and easy to distinguish. In a free hour, when our power of
+            choice is untrammelled and when nothing prevents our being able to do
+            what we like best, every pleasure is to be welcomed and every pain
+            avoided. But in certain circumstances and owing to the claims of duty
+            or the obligations of business it will frequently occur that pleasures
+            have to be repudiated and annoyances accepted. The wise man therefore
+            always holds in these matters to this principle of selection: he
+            rejects pleasures to secure other greater pleasures, or else he
+            endures pains to avoid worse pains."
+          </p>
+        </div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-5xl font-bold text-center">Contact</h1>
+          <p className="text-center">
+            "On the other hand, we denounce with righteous indignation and dislike
+            men who are so beguiled and demoralized by the charms of pleasure of
+            the moment, so blinded by desire, that they cannot foresee the pain
+            and trouble that are bound to ensue; and equal blame belongs to those
+            who fail in their duty through weakness of will, which is the same as
+            saying through shrinking from toil and pain. These cases are perfectly
+            simple and easy to distinguish. In a free hour, when our power of
+            choice is untrammelled and when nothing prevents our being able to do
+            what we like best, every pleasure is to be welcomed and every pain
+            avoided. But in certain circumstances and owing to the claims of duty
+            or the obligations of business it will frequently occur that pleasures
+            have to be repudiated and annoyances accepted. The wise man therefore
+            always holds in these matters to this principle of selection: he
+            rejects pleasures to secure other greater pleasures, or else he
+            endures pains to avoid worse pains."
+          </p>
+        </div>
+      </main>
+    );
+  }
+  
+  export default Contact;
